826da48381d85496ba9e060ce4335713abc8c31e
GitHub Action <action@github.com>
2023-05-31 22:21:45 +0000

    Auto-build and deploy

--- dist/index.js
+++ dist/index.js
@@ -1,31 +1,22 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const tempjs_template_1 = __importDefault(require("tempjs-template"));
 const path_1 = __importDefault(require("path"));
 const fs_1 = __importDefault(require("fs"));
 function getProjects() {
-  const actualPath = path_1.default.resolve(__dirname, "../projects");
-  const files = fs_1.default.readdirSync(actualPath);
-  const directories = files.filter((file) => {
-    const stats = fs_1.default.statSync(`${actualPath}/${file}`);
-    return stats.isDirectory();
-  });
-  return directories;
+    const actualPath = path_1.default.resolve(__dirname, "../projects");
+    const files = fs_1.default.readdirSync(actualPath);
+    const directories = files.filter((file) => {
+        const stats = fs_1.default.statSync(`${actualPath}/${file}`);
+        return stats.isDirectory();
+    });
+    return directories;
 }
-const compiled = tempjs_template_1.default.compileFromFile(
-  path_1.default.resolve(__dirname, "../template/template.html"),
-  {
+const compiled = tempjs_template_1.default.compileFromFile(path_1.default.resolve(__dirname, "../template/template.html"), {
     projects: getProjects(),
-  }
-);
-fs_1.default.writeFileSync(
-  path_1.default.resolve(__dirname, "../index.html"),
-  compiled,
-  "utf-8"
-);
+});
+fs_1.default.writeFileSync(path_1.default.resolve(__dirname, "../index.html"), compiled, "utf-8");
 //# sourceMappingURL=index.js.map
